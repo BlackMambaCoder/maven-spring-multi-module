b9eb102e2623f012bef2dd0e86635c11f2f254e9
Leonhard Radonic <leonhard.radonic@gmail.com>
2017-11-24 11:34:18 +0100

    Dependency management
    Added dependency between web module and service api module. So web module sees only service apis. This dependency should be the only one for the web module to the application core.

--- web/src/main/java/controller/HomeController.java
+++ web/src/main/java/controller/HomeController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import home.HomeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
+    @Autowired
+    private HomeService homeService;
+
     @GetMapping("/home")
     public String getHome() {
-        return "Hello world";
+        return homeService.getHome();
     }
 }
